382fa6a9bc039baaf8ae7b069644b5a1a1f8c97d
nhanvu <nhanvu10092001@icloud.com>
2023-02-12 09:03:21 +0700

    sua loi tren web

--- web.py
+++ web.py
@@ -6,16 +6,9 @@ from PIL import Image
 import numpy as np
 
 # load model and load scaler
-model = joblib.load('model.joblib')
-model_1 = joblib.load('model_1.joblib')
-model_2 = joblib.load('model_2.joblib')
-model_3 = joblib.load('model_3.joblib')
-#model_4 = joblib.load('model_4.joblib')
-model_5 = joblib.load('model_5.joblib')
-model_6 = joblib.load('model_6.joblib')
-
-scaler_1 = joblib.load('scaler_1.joblib')
-scaler_2 = joblib.load('scaler_2.joblib')
+model = joblib.load('defaut.joblib')
+model_modified = joblib.load('modified.joblib')
+scaler = joblib.load('scaler.joblib')
 pca = joblib.load('pca.joblib')
 
 
@@ -42,30 +35,13 @@ if folder is not None:
         st.image(img_resized, caption = 'Image resized to put into model', use_column_width=True)
         img_resized = img_resized.flatten().reshape(1,-1)
 
-    
-
-        image_scaler_1  = scaler_1.transform(img_resized)
-        image_scaler_2  = scaler_2.transform(img_resized)
-
-        image_pca = pca.transform(img_resized)
-
-        image_sp1 = pca.transform(image_scaler_1)
-        image_sp2 = pca.transform(image_scaler_2)
-
         st.text('predict su dung original model')
         st.write(int(model.predict(img_resized)))
 
-        st.text('predict su dung min max scaler ')
-        st.write(int(model_1.predict(image_scaler_1)))
+        image_modified  = scaler.transform(img_resized)
+        
 
-        st.text('predict su dung standard scaler')
-        st.write(int(model_2.predict(image_scaler_2)))
-
-        st.text('predict su dung pca')
-        st.write(int(model_3.predict(image_pca)))
-
-        st.text('predict su dung min max scaler ket hop pca')
-        st.write(int(model_5.predict(image_sp1)))
+        image_modified = pca.transform(image_modified)
 
         st.text('predict su dung standard scaler ket hop pca')
-        st.write(int(model_6.predict(image_sp2)))
+        st.write(int(model_modified.predict(image_modified)))
